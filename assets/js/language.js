80c159bd305a5abb0444703c809228b67b9c9888
RoccoReus <75258357+RoccoReus@users.noreply.github.com>
2023-01-26 18:41:33 +0100

    Add files via upload

---
+++ assets/js/language.js
@@ -0,0 +1,167 @@
+var arrLang = {
+    'en': {
+        'homeknop': 'Homepage',
+        'talenUitschuifKnop': 'Language',
+        'button1': 'Read more',
+        'button2': 'Read more',
+        'button3': 'Read more',
+        'aboutTitel': 'About us',
+        'aboutDescription': 'Since 2019 i live with my husband Robert and 2 sons Rocco & Xander in Koedijk. \n My husband and i both work at the government.\n Since Juli 2019 rent i the Studio above our garage and since the beginning of 2021 the Lodge aswell.\n I really enjoy offering people a pleasant stay and im always open to questions and suggestions or tips.',
+        'directbooking': 'If you want to book directly, you can do that too.',
+        'directbookingbutton': 'Book here!',
+
+        'lodgeButton1': 'Read More',
+        'lodgeOverRuimteTitel': 'About this space',
+        'lodgeOverRuimteDescription': 'Completely new modern, luxurious Lodge with sauna. Just enjoy the peace and space from the living room and the terrace with an unobstructed view of the mill. Relax in your private sauna and cool off outside on the terrace. incl. use of bath towels and bathrobes. Within walking distance you can order food at the Restaurant de Molenschuur. The Lodge is located close to the center of Alkmaar and the beach of Bergen or Egmond. You can also enjoy a lovely walk in the dunes in Schoorl.',
+        'lodgeRuimteTitel': 'The space',
+        'lodgeRuimteDescription': 'The Lodge is located at the back of the garden and you have complete privacy, including a private terrace with a view of the water and the mill and behind the lodge to have something to eat. In the Lodge you will find a living room with sofa and TV, a dining table, a fully equipped kitchen with all kinds of built-in appliances, a separate bedroom with a double bed and spacious closet, a bathroom with shower, toilet and sink and of course a spacious private sauna. There is also an air conditioner.',
+        'lodgeToegangGastenTitel': 'Access for guests',
+        'lodgeToegangGastenDescription': 'Guests of the Lodge can park on our own property at the Lodge parking sign. It is a 15-minute bike ride to the center of Alkmaar and 30 minutes to the beach of Bergen.',
+        'lodgeLetOPTitel': 'Other things to watch out for',
+        'lodgeLetOPDescription': 'The price includes tourist tax and VAT. \n\n We have two 8-speed bicycles that you can rent. The costs for this are 10 euros per bicycle per day. If you have your own bicycles, they can be parked under the carport out of sight or, if desired, in our garage. \n\n In addition, you can rent 2 canoes at 15 euros per canoe at a time. You can also charge your electric car with us. Ask about the costs.',
+        'lodgeButton2': 'Book now',
+
+        'studioButton1': 'Read More',
+        'studioOverRuimteTitel': 'About this space',
+        'studioOverRuimteDescription': 'This beautiful studio is located above the garage with its own entrance under the carport. The studio has a fairytale view of the mill. The studio is not located in the mill! You enter the studio via the stairs. Here you will find a cozy modern atmosphere. You are completely private in the apartment. Behind the apartment is a private terrace with 2 chairs and a table. There are 2 relaxing chairs by the water. You can park your car opposite the mill. Bicycles can be put under the carport.',
+        'studioRuimteTitel': 'The space',
+        'studioRuimteDescription': 'The studio has a private bathroom with a luxurious shower, toilet, sink and hairdryer. There is an air conditioner. There is a small kitchen with fridge, combination microwave, coffee maker, kettle and toaster. There is a safe in the studio. There are also two cushions for the chairs down by the water. Towels, kitchen towels and bed linen are included. Coffee and tea is also available.',
+        'studioToegangGastenTitel': 'Access for guests',
+        'studioToegangGastenDescription': 'The entire studio is at your disposal, including the terrace behind the studio and a lovely spot on the water.',
+        'studioLetOPTitel': 'Other things to watch out for',
+        'studioLetOPDescription': 'If desired, our outdoor Barrel Sauna can be booked for 15 euros per 1-1.5 hours. Towels and bathrobes are included. \n\n In addition, we have two bicycles with 8 gears that you can rent. The costs for this are 10 euros per bicycle per day. If you have your own bicycles, they can be parked under the carport out of sight or, if desired, in our garage. \n\n In addition, you can rent 2 canoes at 15 euros per canoe at a time. Finally, you can rent 2 canoes for 15 euros per canoe per day.',
+        'studioButton2': 'Book now',
+
+        'contactTitel': 'Contact Us',
+        'contactDescription': 'Contact us with the form below',
+        'contactNaam': 'Name',
+        'contactOnderwerp': 'Subject',
+        'contactBericht': 'Enter your message',
+        'contactButton1': 'Send Message',
+        'ContactButton2': 'Reset',
+
+        'succesTitel': 'Successful send!',
+        'succesText1': 'You have successfully sent us a message.',
+        'succesText2': 'U hoort snel van ons.',
+        'succesButton1': 'You will hear from us soon.'
+    },
+    'de': {
+        'homeknop': 'Startseite',
+        'talenUitschuifKnop': 'Sprachen',
+        'button1': 'Weiterlesen',
+        'button2': 'Weiterlesen',
+        'button3': 'Weiterlesen',
+        'aboutTitel': 'über uns',
+        'aboutDescription': 'Ich lebe jetzt seit über drei Jahren mit meinem Mann Robert und den beiden Söhnen Rocco und Xander in Koedijk.\n Mein Mann und ich arbeiten beide bei einer Gemeinde.\n Seit Juli 2019 vermiete ich das Studio über unserer Garage und Anfang 2021 kam die Lodge dazu.\n Ich genieße es sehr, Menschen einen angenehmen Aufenthalt zu bieten und bin für Fragen und Anregungen oder Tipps immer offen.',
+        'directbooking': 'Wenn Sie direkt buchen möchten, können Sie das auch tun.',
+        'directbookingbutton': 'Mach das hier!',
+
+        'lodgeButton1': 'Weiterlesen',
+        'lodgeOverRuimteTitel': 'Über diesen Raum',
+        'lodgeOverRuimteDescription': 'Völlig neue moderne, luxuriöse Lodge mit Sauna. Genießen Sie einfach die Ruhe und Weite vom Wohnzimmer und der Terrasse mit freiem Blick auf die Mühle. Entspannen Sie sich in Ihrer privaten Sauna und kühlen Sie sich draußen auf der Terrasse ab. inkl. Benutzung von Badetüchern und Bademänteln. In Gehweite können Sie im Restaurant de Molenschuur Essen bestellen. Die Lodge liegt in der Nähe des Zentrums von Alkmaar und des Strandes von Bergen oder Egmond. Sie können auch einen schönen Spaziergang in den Dünen von Schoorl machen.',
+        'lodgeRuimteTitel': 'Der Raum',
+        'lodgeRuimteDescription': 'Die Lodge befindet sich auf der Rückseite des Gartens und Sie haben absolute Privatsphäre, einschließlich einer privaten Terrasse mit Blick auf das Wasser und die Mühle und hinter der Lodge, um etwas zu essen. In der Lodge finden Sie ein Wohnzimmer mit Sofa und Fernseher, einen Esstisch, eine voll ausgestattete Küche mit allerlei Einbaugeräten, ein separates Schlafzimmer mit Doppelbett und geräumigem Schrank, ein Badezimmer mit Dusche, WC und Waschbecken und natürlich eine großzügige private Sauna. Es gibt auch eine Klimaanlage.',
+        'lodgeToegangGastenTitel': 'Zugang für Gäste',
+        'lodgeToegangGastenDescription': 'Gäste der Lodge können auf unserem eigenen Grundstück am Lodge-Parkschild parken. Es ist eine 15-minütige Radtour zum Zentrum von Alkmaar und 30 Minuten zum Strand von Bergen.',
+        'lodgeLetOPTitel': 'Andere Dinge, auf die Sie achten sollten',
+        'lodgeLetOPDescription': 'Der Preis beinhaltet Kurtaxe und Mehrwertsteuer. Wir haben zwei 8-Gang-Fahrräder, die Sie mieten können. Die Kosten dafür betragen 10 Euro pro Fahrrad und Tag. Eigene Fahrräder können unsichtbar unter dem Carport oder auf Wunsch in unserer Garage abgestellt werden. Außerdem können Sie 2 Kanus für jeweils 15 Euro pro Kanu mieten. Auch Ihr Elektroauto können Sie bei uns aufladen. Fragen Sie nach den Kosten.',
+        'lodgeButton2': 'Bücher',
+
+        'studioButton1': 'Weiterlesen',
+        'studioOverRuimteTitel': 'Über diesen Raum',
+        'studioOverRuimteDescription': 'Dieses schöne Studio befindet sich über der Garage mit eigenem Eingang unter dem Carport. Das Studio hat einen märchenhaften Blick auf die Mühle. Das Studio befindet sich nicht in der Mühle! Sie betreten das Studio über die Treppe. Hier finden Sie eine gemütliche moderne Atmosphäre. In der Wohnung sind Sie ganz privat. Hinter der Wohnung befindet sich eine private Terrasse mit 2 Stühlen und einem Tisch. Am Wasser stehen 2 Liegestühle und gegenüber der Mühle können Sie Ihr Auto parken. Fahrräder können unter dem Carport abgestellt werden.',
+        'studioRuimteTitel': 'Der Raum',
+        'studioRuimteDescription': 'Das Studio verfügt über ein eigenes Badezimmer mit luxuriöser Dusche, Toilette, Waschbecken und Haartrockner. Es gibt eine Klimaanlage. Es gibt eine kleine Küche mit Kühlschrank, Kombi-Mikrowelle, Kaffeemaschine, Wasserkocher und Toaster. Es gibt einen Safe im Studio. Es gibt auch zwei Kissen für die Stühle unten am Wasser. Handtücher, Küchentücher und Bettwäsche sind inklusive. Kaffee und Tee sind ebenfalls erhältlich.',
+        'studioToegangGastenTitel': 'Zugang für Gäste',
+        'studioToegangGastenDescription': 'Das gesamte Studio steht Ihnen zur Verfügung, einschließlich der Terrasse hinter dem Studio und einem schönen Platz am Wasser.',
+        'studioLetOPTitel': 'Andere Dinge, auf die Sie achten sollten',
+        'studioLetOPDescription': 'Auf Wunsch kann unsere Fasssauna im Freien für 15 Euro pro 1-1,5 Stunden gebucht werden. Handtücher und Bademäntel sind inklusive. Außerdem haben wir zwei Fahrräder mit 8 Gängen, die Sie mieten können. Die Kosten dafür betragen 10 Euro pro Fahrrad und Tag. Eigene Fahrräder können unsichtbar unter dem Carport oder auf Wunsch in unserer Garage abgestellt werden. Außerdem können Sie 2 Kanus für jeweils 15 Euro pro Kanu mieten. Schließlich können Sie 2 Kanus für 15 Euro pro Kanu und Tag mieten.',
+        'studioButton2': 'Bücher',
+
+        'contactTitel': 'Kontaktiere uns',
+        'contactDescription': 'Kontaktieren Sie uns mit dem untenstehenden Formular',
+        'contactNaam': 'Name',
+        'contactOnderwerp': 'Gegenstand',
+        'contactBericht': 'Schreiben Sie ihre Nachricht hier',
+        'contactButton1': 'Nachricht senden',
+        'ContactButton2': 'Zurücksetzen',
+
+        'succesTitel': 'Erfolgreich gesendet!',
+        'succesText1': 'Sie haben uns erfolgreich eine Nachricht gesendet.',
+        'succesText2': 'Sie werden bald von uns hören.',
+        'succesButton1': 'Zurück zur Startseite'
+    },
+    'nl': {
+        'homeknop': 'Homepagina',
+        'talenUitschuifKnop': 'Talen',
+        'button1': 'Lees meer',
+        'button2': 'Lees meer',
+        'button3': 'Lees meer',
+        'aboutTitel': 'Over Ons',
+        'aboutDescription': 'Sinds ruim drie jaar woon ik met mijn man Robert en twee zoons Rocco & Xander in Koedijk.\n Mijn man en ik werken beiden bij een gemeente.\n Sinds juli 2019 verhuur ik de studio boven onze garage en begin 2021 is daar de Lodge bij gekomen.\n Ik vind het ontzettend leuk om mensen een prettig verblijf te bieden en sta altijd open voor vragen en voor suggesties of tips.',
+        'directbooking': 'Wilt nu rechstreeks boeken dan kan dat ook.',
+        'directbookingbutton': 'Doe dat hier!',
+
+        'lodgeButton1': 'Lees meer',
+        'lodgeOverRuimteTitel': 'Over deze ruimte',
+        'lodgeOverRuimteDescription': 'Compleet nieuwe moderne, luxe Lodge met sauna. Even heerlijk genieten van de rust en de ruimte met vanuit de woonkamer en het terras met vrij uitzicht op de molen. Heerlijk relaxen in je privé sauna en afkoelen lekker buiten op het terras. Incl. gebruik van badlakens en badjassen. Op loopafstand eten te bestellen bij de Restaurant de Molenschuur. De Lodge ligt dichtbij het centrum van Alkmaar en het strand van Bergen of Egmond. Wandel ook heerlijk in de duinen in Schoorl.',
+        'lodgeRuimteTitel': 'De ruimte',
+        'lodgeRuimteDescription': 'De Lodge ligt achter in de tuin en je hebt er volledige privacy inclusief eigen terras met zicht op het water en de molen en achter de lodge om wat te eten. In de Lodge tref je een woonkamer met bank en TV, een eettafel, een compleet uitgeruste keuken met allerlei inbouw apparatuur, een aparte slaapkamer met tweepersoonsbed en ruime kast, een badkamer met douche, toilet en wastafel en natuurlijk een ruime privé sauna. Er is ook een airco aanwezig.',
+        'lodgeToegangGastenTitel': 'Toegang voor gasten',
+        'lodgeToegangGastenDescription': 'Gasten van de Lodge kunnen parkeren op ons eigen terrein bij het parkeerbordje Lodge. Het is 15 minuten fietsen naar het centrum van Alkmaar en 30 minuten naar het strand van Bergen.',
+        'lodgeLetOPTitel': 'Andere dingen waar je op moet letten',
+        'lodgeLetOPDescription': 'De prijs in incl. toeristenbelasting en btw. \n\n Wij hebben twee fietsen met 8 versnellingen die u kunt huren. De kosten hiervoor bedragen 10 euro per fiets per dag. Heeft u eigen fietsen dan kunnen deze onder de carport uit het zicht geparkeerd worden of indien gewenst in onze garage. \n\n Daarnaast kunt u 2 kanos huren à 15 euro per kano per keer. U kunt ook uw elektrische auto bij ons opladen. Vraag naar de kosten.',
+        'lodgeButton2': 'Boeken',
+
+        'studioButton1': 'Lees meer',
+        'studioOverRuimteTitel': 'Over deze ruimte',
+        'studioOverRuimteDescription': 'Deze prachtige studio ligt boven de garage met eigen opgang onder de carport. De studio heeft een sprookjesachtig uitzicht op de molen. De studio bevindt zich niet in de molen! Via de trap komt u in de studio. Hier treft een knusse moderne sfeer. In het appartement bent u volledig privé. Achter het appartement is een eigen privé terras met 2 stoelen en een tafel. Bij het water staan 2 relaxstoelen Uw auto kunt u parkeren tegenover de molen. Fietsen kunnen onder de carport gezet worden.',
+        'studioRuimteTitel': 'De ruimte',
+        'studioRuimteDescription': 'De studio beschikt over een eigen badkamer met luxe douche, toilet, wastafel en fohn. Er is een airco aanwezig. Er is een kleine keuken met koelkast, combimagnetron, koffiezetapparaat, waterkoker en broodrooster. In de studio zit een kluisje. Verder liggen er twee kussen voor in de stoelen beneden bij het water. Handdoeken, keukendoeken en beddengoed is inbegrepen. Ook is koffie en thee aanwezig.',
+        'studioToegangGastenTitel': 'Toegang voor gasten',
+        'studioToegangGastenDescription': 'De gehele studio staat tot uw beschikking incl. het terras achter de studio en een heerlijk plekje aan het water.',
+        'studioLetOPTitel': 'Andere dingen waar je op moet letten',
+        'studioLetOPDescription': 'Indien gewenst kan onze buiten Barrelsauna bijgeboekt worden voor 15 euro per 1-1,5 uur. Handdoeken en badjassen zijn inbegrepen. \n\n Daarnaast hebben wij twee fietsen met 8 versnellingen die u kunt huren. De kosten hiervoor bedragen 10 euro per fiets per dag. Heeft u eigen fietsen dan kunnen deze onder de carport uit het zicht geparkeerd worden of indien gewenst in onze garage.\n \n\n Daarnaast kunt u 2 kanos huren à 15 euro per kano per keer. Tenslotte kunt u 2 kanos huren voor 15 euro per kano per dag.',
+        'studioButton2': 'Boeken',
+
+        'contactTitel': 'Contacteer Ons',
+        'contactDescription': 'Contacteer ons met het formulier hieronder',
+        'contactNaam': 'Naam',
+        'contactOnderwerp': 'Onderwerp',
+        'contactBericht': 'Type hier u bericht',
+        'contactButton1': 'Stuur bericht',
+        'ContactButton2': 'Reset',
+
+        'succesTitel': 'Succesvol verstuurd!',
+        'succesText1': 'U heeft succesvol een bericht naar ons verstuur.',
+        'succesText2': 'U hoort snel van ons.',
+        'succesButton1': 'Terug naar homepagina'
+    }
+}
+
+function translateLang(lang)
+{
+    $('.lang').each(function(index, item) {
+        $(this).text(arrLang[lang][$(this).attr('key')]);
+    });
+}
+
+$(function() {
+    //first check for stored language in localStorage i.e. fetch data from localStorage
+    let stored_lang = localStorage.getItem("stored_lang");
+    //if any then translate page accordingly
+    if(stored_lang != null)
+    {
+        lang = stored_lang;
+        translateLang(lang);
+    }
+
+
+    $('.translate').click(function() {
+        var lang = $(this).attr('id');
+        //on click store language to localStorage
+        localStorage.setItem("stored_lang",lang);
+        translateLang(lang);
+    });
+
+});
--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,19 +1,34 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
+var slides,dots;
 
 function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex> slides.length) {slideIndex = 1}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
+
+function plusSlides(position) {
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
