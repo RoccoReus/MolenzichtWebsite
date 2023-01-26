9b42e08ed54973ad393317e47accde18312e5d6e
RoccoReus <75258357+RoccoReus@users.noreply.github.com>
2023-01-26 20:48:27 +0100

    Add files via upload

---
+++ openlink.js
@@ -0,0 +1,9 @@
+function UserNewTab1(url) {
+    var win = window.open("https://www.airbnb.nl/rooms/46997940?source_impression_id=p3_1651444134_0Zhw67SiVMb2jsD2", '_blank');
+    win.focus();
+}
+
+function UserNewTab2(url) {
+    var win = window.open("https://www.airbnb.nl/rooms/25449919?source_impression_id=p3_1651444140_Pl78pZHZFHY%2ByK9a", '_blank');
+    win.focus();
+}
